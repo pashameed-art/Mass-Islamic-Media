d58e8f098258e3692401f066ff7debc1ac2bdb20
pashameed-art <pashameed@gmail.com>
2026-08-26 00:40:24 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,51 +1,12 @@
-const CACHE_NAME = 'mass-islamic-media-v18-7';
-
-const APP_SHELL = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png',
-  './maskable-icon-512.png',
-  './apple-touch-icon.png'
-];
-
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(APP_SHELL))
-      .then(() => self.skipWaiting())
-  );
-});
-
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(
-        keys
-          .filter((key) => key !== CACHE_NAME)
-          .map((key) => caches.delete(key))
-      )
-    ).then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener('fetch', (event) => {
-  if (event.request.method !== 'GET') return;
-
-  event.respondWith(
-    fetch(event.request)
-      .then((response) => {
-        const copy = response.clone();
-        caches.open(CACHE_NAME)
-          .then((cache) => cache.put(event.request, copy))
-          .catch(() => {});
-        return response;
-      })
-      .catch(() =>
-        caches.match(event.request).then(
-          (cached) => cached || caches.match('./index.html')
-        )
-      )
-  );
+const CACHE_NAME='mass-islamic-media-v18-7';
+const APP_SHELL=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./maskable-icon-512.png','./apple-touch-icon.png'];
+self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(APP_SHELL)).then(()=>self.skipWaiting()));});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
+self.addEventListener('fetch',event=>{
+ if(event.request.method!=='GET')return;
+ event.respondWith(fetch(event.request).then(response=>{
+   const copy=response.clone();
+   caches.open(CACHE_NAME).then(c=>c.put(event.request,copy)).catch(()=>{});
+   return response;
+ }).catch(()=>caches.match(event.request).then(r=>r||caches.match('./index.html'))));
 });
