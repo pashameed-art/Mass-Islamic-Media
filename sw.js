d1461803badd37543e55bb6ad17fc21e3811c5c3
pashameed-art <pashameed@gmail.com>
2026-08-26 00:46:00 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='mass-islamic-media-v18-7';
+const CACHE_NAME='mass-islamic-media-v19-5';
 const APP_SHELL=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./maskable-icon-512.png','./apple-touch-icon.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(APP_SHELL)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
